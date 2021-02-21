2a7f8bbbf4945358c71b45e7adaf9b2aa516d4c7
JamesWeight <JamesWeight>
2021-02-21 01:46:33 -0800

    2021feb21am146

---
+++ Build/InventoryManagementSolution/MLIMaController.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace Controller
+{
+    public class ControllerRoutines
+    {
+        public void Display()
+        {
+            Console.WriteLine("TDD-StructuralIntegrityController");
+       }
+    }
+}
+
+// Revature Intellectual Property and Resource:
+//  Batch -> 2102-feb08-net
+//
+//  Project0 - Feb 12, 2021 Received Original Specifications
+//           - Feb 18, 2021 Received Requested Specifications
+//           - Feb 24, 2021 Requested Deliverable
+//
+//  Project Lead - Revature Corporate Trainor Nick Escalon
+//  Project Deliverable - Developer & Author -
+//                        Revature Engineering Associate James Weight
---
+++ Build/MicrosoftLogging-TroubleShootingDebuggingExtensions/MicrosoftExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ExtensionMS
+{
+    // Added a Unit Test
+    public class ExtensionMSroutines
+    {   
+       // Structural Integrity Test
+        public void Display()
+        {
+            Console.WriteLine("TDD-StructuralIntegrity-ExtensionMS");
+        }
+        // Required
+        static void Main(){ }
+    }
+}
+
+// Revature Intellectual Property and Resource:
+//  Batch -> 2102-feb08-net
+//
+//  Project0 - Feb 12, 2021 Received Original Specifications
+//           - Feb 18, 2021 Received Requested Specifications
+//           - Feb 24, 2021 Requested Deliverable
+//
+//  Project Lead - Revature Corporate Trainor Nick Escalon
+//  Project Deliverable - Developer & Author -
+//                        Revature Engineering Associate James Weight
