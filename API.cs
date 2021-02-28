099ab4b11632b43de91beb645c9865aad20369ed
JamesWeight <JamesWeight>
2021-02-28 00:11:29 -0800

    Solid Construction - Next: SQL with ORM - 2021feb28am1211

--- API.cs
+++ API.cs
@@ -17,27 +17,33 @@ namespace Application
         {   
             // Connection Information
             string Pointer = File.ReadAllText("./MLiMa.Info");
-            Console.WriteLine(Pointer);
-            // End Program & Close Connections
-            // bool Complete = false;
+            //
+            // Console.WriteLine(Pointer); // Test
+            //
+            // Register to End Program & Close Connections
+            bool Complete = false;
             // Establish DB
-             SqlConnection DBconnect = new SqlConnection(Pointer);
-
-             DBconnect.Open();
+            SqlConnection DBconnect = new SqlConnection(Pointer);
+            // Open ResourceDB
+            DBconnect.Open();
+            // Title
+            Console.WriteLine(" " + (char)13 + (char)10 + " ");
+            string _data = "Welcome to the Market Locations Inventory Management Application";
+            Console.WriteLine(_data + (char)13 + (char)10);
+            // Display Menu
+            DisplayMenu(_data);
             // Main Iteration of User Interface
-            /*
             do
             {
-                // checked for Key Pressed as ASCII
+                // check for Key Pressed as ASCII
                 int Key = Console.Read();
                 // Flow-Chart Pointers
-                if (Key == 48) Complete = true; 
+                if (Key == 48) Complete = true;
+                if (Key == 57) DisplayMenu(_data);
             }
             while (!Complete);
-            */
-            // Console.WriteLine(Complete);
-            // Close Resources
-             DBconnect.Close();
+            // Close ResourceDB
+            DBconnect.Close();
             // Structural Integrity Test
             /*
             CustomerLibrary.Routines _instantiateView1 = new CustomerLibrary.Routines();
@@ -50,6 +56,30 @@ namespace Application
             OrderLibrary.Routines _instantiateView0 = new OrderLibrary.Routines();
             _instantiateView0.Interface();
             */
+        }
+        // Display Menu Method
+        static void DisplayMenu(string Data)
+        {
+            // Menu
+            Console.WriteLine
+            (
+                new String('=', Data.Length) + (char)13 + (char)10
+                + (char)13 + (char)10              
+                + "Choose your task via KeyPress and then Press 'Enter'." + (char)13 + (char)10
+                + (char)13 + (char)10              
+                + "1) Order" + (char)13 + (char)10
+                + "2) Add Customer" + (char)13 + (char)10
+                + "3) Search Customers by Name" + (char)13 + (char)10
+                + "4) Display Details of an Order" + (char)13 + (char)10              
+                + "5) Display History of Orders at a Store" + (char)13 + (char)10 
+                + "6) Display History of a Customer" + (char)13 + (char)10
+                + new String('=', Data.Length) + (char)13 + (char)10
+                + "A) Serialize Data" + (char)13 + (char)10
+                + "B) DeSerialize Data" + (char)13 + (char)10
+                + new String('-', Data.Length) + (char)13 + (char)10
+                + "0) Exit" + (char)13 + (char)10
+                + "9) Redisplay Menu" + (char)13 + (char)10
+            );
         }
     }
 }
